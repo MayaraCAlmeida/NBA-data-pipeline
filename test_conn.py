343aa8fc66ac9e802da7bf28557da7857b1ef622
Mayara Almeida <mayalmeida222@gmail.com>
2026-03-26 21:42:29 -0300

    testando conexão com neon

---
+++ test_conn.py
@@ -0,0 +1,15 @@
+import psycopg2, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    sslmode="require",  # ← isso
+)
+print("conectou!")
+conn.close()
